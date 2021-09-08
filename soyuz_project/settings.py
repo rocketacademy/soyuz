d8597520e8b3662cc1df2ca09911592ebc28f2aa
awongh <akira@awongh.com>
2021-09-08 20:26:47 +0800

    add heroku test deploy url to allowed hosts

--- soyuz_project/settings.py
+++ soyuz_project/settings.py
@@ -25,9 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['soyuz-ra.herokuapp.com','127.0.0.1']
 
 # Application definition
 
