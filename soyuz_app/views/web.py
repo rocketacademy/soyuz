21827afa74aa83632d1691735669ab2d6307bc10
michelle-mok <michellemokhuihui@gmail.com>
2021-10-22 09:03:06 +0800

    change user section/ delete user from section

--- soyuz_app/views/web.py
+++ soyuz_app/views/web.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 from ..forms import AddBatchForm
@@ -36,6 +37,29 @@ def get_sections(request, batch_id):
         section_users = section.users.all()
         section_obj["users"] = section_users
         section_array.append(section_obj)
+
+    if "delete_items" in request.POST:
+        # Fetch user id and section name of user we want to remove from a section
+        user_to_delete = request.POST.get("delete_items")
+        user_to_delete = user_to_delete.split("/")
+        # section that user is in
+        selected_section = Section.objects.get(number=int(user_to_delete[0]))
+        # user that we want to delete
+        user = get_user_model().objects.get(id=int(user_to_delete[1]))
+        selected_section.users.remove(user)
+
+        # fetch destinaton section number and user id
+    elif "batch_sections" in request.POST:
+        section_to_move = request.POST["batch_sections"]
+        section_to_move = section_to_move.split("/")
+        # user that we want to move
+        selected_user = get_user_model().objects.get(id=int(section_to_move[1]))
+        # user's original section
+        user_section = Section.objects.get(users__id=int(section_to_move[1]))
+        # user's destination section
+        destination_section = Section.objects.get(number=int(section_to_move[0]))
+        user_section.users.remove(selected_user)
+        destination_section.users.add(selected_user)
 
     context = {
         "title": "List of Sections",
