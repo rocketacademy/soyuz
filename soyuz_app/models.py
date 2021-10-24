238fe0316f8e8d0c3ac9ea3198c6970a83613014
michelle-mok <michellemokhuihui@gmail.com>
2021-10-25 04:35:27 +0800

    added first and last names to user model

--- soyuz_app/models.py
+++ soyuz_app/models.py
@@ -10,13 +10,23 @@ from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
-
-    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+    def _create_user(
+        self,
+        first_name,
+        last_name,
+        email,
+        password,
+        is_staff,
+        is_superuser,
+        **extra_fields
+    ):
         if not email:
-            raise ValueError('Userz must have an email address')
+            raise ValueError("Userz must have an email address")
         now = timezone.now()
         email = self.normalize_email(email)
         user = self.model(
+            first_name=first_name,
+            last_name=last_name,
             email=email,
             is_staff=is_staff,
             is_active=True,
@@ -29,7 +39,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, first_name, last_name, email, password, **extra_fields):
         return self._create_user(email, password, False, False, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
@@ -38,6 +48,8 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     email = models.EmailField(max_length=254, unique=True)
     hubspot_id = models.CharField(max_length=200, null=True, blank=True)
     github_username = models.CharField(max_length=200, null=True, blank=True)
@@ -47,9 +59,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_login = models.DateTimeField(null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     objects = UserManager()
 
@@ -57,7 +69,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         return "/users/%i/" % (self.pk)
 
 
-class Course (models.Model):
+class Course(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
@@ -67,23 +79,21 @@ class Course (models.Model):
 class Batch(models.Model):
     number = models.IntegerField()
     start_date = models.DateField()
-    users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, blank=True)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
     course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
 
 
-class Section (models.Model):
+class Section(models.Model):
     number = models.IntegerField()
-    users = models.ManyToManyField(
-        settings.AUTH_USER_MODEL, blank=True)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
     batch_id = models.ForeignKey(Batch, on_delete=models.CASCADE)
 
 
-class Workflow_type (models.Model):
+class Workflow_type(models.Model):
     name = models.CharField(max_length=200)
     course_id = models.ManyToManyField(Course)
 
 
-class Workflows (models.Model):
+class Workflows(models.Model):
     workFlow_type_id = models.ForeignKey(Workflow_type, on_delete=PROTECT)
     completed = models.BooleanField(default=False)
