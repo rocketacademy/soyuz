cc3aa81d158f7356719a13aefae582cf46d73be8
michelle-mok <michellemokhuihui@gmail.com>
2022-01-11 18:15:52 +0800

    youtube test code

--- soyuz_app/views/zoom.py
+++ soyuz_app/views/zoom.py
@@ -1,7 +1,10 @@
 import json
+import os
+
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from .youtube import main
 
 ZOOM_API_KEY = settings.ZOOM_API_KEY
 ZOOM_API_SECRET = settings.ZOOM_API_SECRET
@@ -10,7 +13,7 @@ ZOOM_API_SECRET = settings.ZOOM_API_SECRET
 @csrf_exempt
 def zoom_webhook(request):
     json_dict = json.loads(request.body.decode('utf-8'))
-
+    print('json dict', json_dict)
     # if recording is complete
     if json_dict['event'] == 'recording.completed':
         recording_files = json_dict['payload']['object']['recording_files'][0]
@@ -19,5 +22,7 @@ def zoom_webhook(request):
         play_url = recording_files['play_url']
         print('download_url', download_url)
         print('play url', play_url)
+
+        main(download_url)
 
     return HttpResponse(status=200)
